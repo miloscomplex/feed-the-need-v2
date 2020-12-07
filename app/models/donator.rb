2de655aaece48f48ccc67f138cb502e01df0ef38
miloscomplex <hicallmesutton@gmail.com>
2020-12-06 23:10:12 -0600

    Added model relationships

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -1,6 +1,7 @@
 class Donator < ApplicationRecord
   has_secure_password
-
+  has_many :items
+  
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
   validates :password, confirmation: true
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
 
+  belongs_to :needy
+  belongs_to :donator 
+
   validates :name, :quantity, presence: true
 
 end
